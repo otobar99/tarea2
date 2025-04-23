4edc487fa6071214fc5f56cbee4f13bfd7ec58d5
Oscar Tobar <oscar.tobar@morapavic.cl>
2025-04-22 20:33:30 -0400

    hello world

---
+++ src/main/java/com/mycompany/tarea2/Tarea2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
